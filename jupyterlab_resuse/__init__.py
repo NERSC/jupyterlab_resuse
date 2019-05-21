8e4fe7f250b72d1dcbc09daf88c962aeb3b966c2
William Krinsman <krinsman@berkeley.edu>
2019-05-21 16:49:44 -0700

    first commit

---
+++ jupyterlab_resuse/__init__.py
@@ -0,0 +1,33 @@
+"""JupyterLab Resuse : view and manage resource usage from JupyterLab tab """
+
+from .resuse import MetricsHandler
+from .resuse import ResourceUseDisplay
+
+from notebook.utils import url_path_join
+
+__version__="0.1.1"
+
+def _jupyter_server_extension_paths():
+    return [{
+        "module": "jupyterlab_resuse"
+        }]
+        
+def load_jupyter_server_extension(nb_server_app):
+    """
+    Called when the extension is loaded.
+
+    Args:
+        nb_server_app (NotebookWebApplication): handle to the Notebook webserver instance.
+    """
+    print('jupyterlab_resuse server extension loaded')
+    resuseconfig = ResourceUseDisplay(parent=nb_server_app)
+    web_app = nb_server_app.web_app
+    host_pattern = '.*$'
+
+    web_app.settings['nbresuse_display_config'] = resuseconfig
+    base_url = web_app.settings['base_url']
+
+    web_app.add_handlers(host_pattern, [
+        (url_path_join(base_url, '/metrics'), MetricsHandler)
+        ])
+
