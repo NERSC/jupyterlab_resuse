9e344b595d9170ae97d92fe30853d7965fe3e5fb
William Krinsman <krinsman@berkeley.edu>
2019-08-08 17:49:17 -0700

    Removed all Python parts since they are now made irrelevant by my update to my fork of nbresuse.

--- __init__.py
+++
@@ -1,6 +0,0 @@
-def mem_limit_calculator(rss, total_mem, used_mem, cpu_percent, num_users):
-    if num_users > 0:
-        targetPctFraction = 1/num_users
-    else:
-        targetPctFraction = 1
-    return targetPctFraction * total_mem
--- jupyterlab_resuse/__init__.py
+++
@@ -1,8 +0,0 @@
-__version__="0.1.2"
-
-def mem_limit_calculator(rss, used_mem, total_mem, num_users, cpu_percent):
-    if num_users > 0:
-        targetPctFraction = 1/num_users
-    else:
-        targetPctFraction = 1
-    return targetPctFraction * total_mem
